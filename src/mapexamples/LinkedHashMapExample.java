35f735b0fac295607e8752177c8fed8baa23ced9
Chandan K M <chandanchintu16@gmail.com>
2025-01-13 21:28:10 +0530

    addes some lines

--- src/mapexamples/LinkedHashMapExample.java
+++ src/mapexamples/LinkedHashMapExample.java
@@ -22,6 +22,7 @@ public class LinkedHashMapExample {
         map1.put(101,"Watermelon");// recent value is taken
         map1.put(null,null);
 
+        System.out.println("added code");
         System.out.println("map1 is : "+map1);
 
         System.out.println("get element at 102 : "+map1.get(102));
@@ -37,6 +38,5 @@ public class LinkedHashMapExample {
         for (Map.Entry m1: map1.entrySet()){
             System.out.println(m1);
         }
-
     }
 }
